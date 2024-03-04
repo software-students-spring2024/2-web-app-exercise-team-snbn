cc990856be39b351b402e78a960fe316548e4147
Nmeng01 <ndm9914@nyu.edu>
2024-03-04 08:57:55 -0500

    final changes

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ load_dotenv()
 uri = os.getenv('MONGO_URI')
 
 client = MongoClient(uri)
-db = client[os.getenv('MONGO_DBNAME')]  
-collection = db[os.getenv('MONGO_COURSES')]  
-commentsColl = db[os.getenv('MONGO_COMMENTS')]  
+db = client[os.getenv('MONGO_DBNAME')]
+collection = db['coursesCollection']  
+commentsColl = db['comments'] 
 
 
 #Start Flask server and have routes for each endpoint. 
@@ -25,55 +25,55 @@ app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def home():
 
-    return render_template("index.html");
+    return render_template("index.html")
 
 @app.route("/course_search", methods=['GET'])
 def courseSearch():
      
-    search = request.args.get('q');
+    search = request.args.get('q')
 
     if(not search):
-      data = collection.find();
-      return render_template("CourseSearch.html",data = data);
+      data = collection.find()
+      return render_template("CourseSearch.html",data = data)
     else:
-      collection.create_index([('classTitle', 'text'), ('classNumber',  'text'),('schedule', 'text'),('notes', 'text')]);
+      collection.create_index([('classTitle', 'text'), ('classNumber',  'text'),('schedule', 'text'),('notes', 'text')])
       data = collection.find({"$text": {"$search": parse.unquote(search)}})
-      return render_template("CourseSearch.html",data = data);
+      return render_template("CourseSearch.html",data = data)
 
 
 
 @app.route("/view_comments", methods=['GET','PUT'])
 def viewComments():
-     data = commentsColl.find();
-     newData= [];
+     data = commentsColl.find()
+     newData= []
      for doc in data:
 
-       objInstance = ObjectId(doc['forID']);
+       objInstance = ObjectId(doc['forID'])
        classCurrent = collection.find_one({"_id": objInstance})
-       doc["articleTitle"] = classCurrent['classTitle'];
-       doc["classNumber"] = classCurrent['classNumber'];
-       doc["schedule"] = classCurrent['schedule'];
-       newData.append(doc);
+       doc["articleTitle"] = classCurrent['classTitle']
+       doc["classNumber"] = classCurrent['classNumber']
+       doc["schedule"] = classCurrent['schedule']
+       newData.append(doc)
      return render_template("ViewComments.html",data=newData)
 
 @app.route("/delete_comments/<commentID>", methods=['GET','DELETE'])
 def deleteComments(commentID):
      
      if request.method == 'DELETE':
-          objDelete = ObjectId(commentID);
+          objDelete = ObjectId(commentID)
           commentsColl.delete_one({"_id": objDelete})
           return redirect("/view_comments", code=302)
 
              
  
-     objInstance = ObjectId(commentID);
+     objInstance = ObjectId(commentID)
      data = commentsColl.find_one({"_id": objInstance})
 
-     objInstance2 = ObjectId(data['forID']);
+     objInstance2 = ObjectId(data['forID'])
      articleTitle = collection.find_one({"_id": objInstance2})
-     data["articleTitle"] = articleTitle['classTitle'];
-     data["classNumber"] = articleTitle['classNumber'];
-     data["schedule"] = articleTitle['schedule'];
+     data["articleTitle"] = articleTitle['classTitle']
+     data["classNumber"] = articleTitle['classNumber']
+     data["schedule"] = articleTitle['schedule']
 
 
 
@@ -82,21 +82,21 @@ def deleteComments(commentID):
 @app.route("/edit_comments/<commentID>", methods=['GET','PUT'])
 def editComments(commentID):
      if request.method == 'PUT':
-          print(request.json);
-          objEdit = ObjectId(commentID);
+          print(request.json)
+          objEdit = ObjectId(commentID)
 
           commentsColl.update_one({"_id": objEdit}, { "$set": { "name": request.json['newName'],"text": request.json['newCom'] } })
 
-          return redirect("/view_comments", code=302);
+          return redirect("/view_comments", code=302)
  
-     objInstance = ObjectId(commentID);
+     objInstance = ObjectId(commentID)
      data = commentsColl.find_one({"_id": objInstance})
 
-     objInstance2 = ObjectId(data['forID']);
+     objInstance2 = ObjectId(data['forID'])
      articleTitle = collection.find_one({"_id": objInstance2})
-     data["articleTitle"] = articleTitle['classTitle'];
-     data["classNumber"] = articleTitle['classNumber'];
-     data["schedule"] = articleTitle['schedule'];
+     data["articleTitle"] = articleTitle['classTitle']
+     data["classNumber"] = articleTitle['classNumber']
+     data["schedule"] = articleTitle['schedule']
 
 
 
@@ -119,7 +119,7 @@ def showClass(classId):
         })
 
         
-     objInstance = ObjectId(classId);
+     objInstance = ObjectId(classId)
      data = collection.find_one({"_id": objInstance})
      comments = commentsColl.find({"forID": classId})
      return render_template("showClass.html", data=data, comments=comments)
